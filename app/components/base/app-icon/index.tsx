4aaa4e9b88031d4fae90976f2a1eff98c16eae38
xy3 <120182408@qq.com>
2023-08-31 09:45:40 +0800

    更新顶部icon

--- app/components/base/app-icon/index.tsx
+++ app/components/base/app-icon/index.tsx
@@ -3,12 +3,11 @@ import classNames from 'classnames'
 import style from './style.module.css'
 
 export type AppIconProps = {
-  size?: 'tiny' | 'small' | 'medium' | 'large'
-  rounded?: boolean
-  icon?: string
-  background?: string
-  className?: string
-}
+  size?: 'tiny' | 'small' | 'medium' | 'large';
+  rounded?: boolean;
+  background?: string;
+  className?: string;
+};
 
 const AppIcon: FC<AppIconProps> = ({
   size = 'medium',
@@ -28,9 +27,29 @@ const AppIcon: FC<AppIconProps> = ({
         background,
       }}
     >
-
     </span>
-  )
-}
-
+  );
+};
+// const AppIcon: FC<AppIconProps> = ({
+//   size = 'medium',
+//   rounded = false,
+//   background,
+//   className,
+// }) => {
+//   return (
+//     <span
+//       className={classNames(
+//         style.appIcon,
+//         size !== 'medium' && style[size],
+//         rounded && style.rounded,
+//         className ?? '',
+//       )}
+//       style={{
+//         background,
+//       }}
+//     >
+//       🤖
+//     </span>
+//   )
+// }
 export default AppIcon
